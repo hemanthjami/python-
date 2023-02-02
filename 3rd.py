80bad878c9c9d821ebe7547be4c1ca7d52a48548
hemanthjami <112924792+hemanthjami@users.noreply.github.com>
2023-02-02 14:21:51 +0530

    Add files via upload

---
+++ 3rd.py
@@ -0,0 +1,6 @@
+COuntofWords = len ("Hello This is python Programming" .split 0)
+print ("Count of Words in the given Sentence : ", countofWords)
+
+print (Len ("Hello This is python Programming" . split () ))
+
+print(len Cinput ("Enter put: ") .split )
